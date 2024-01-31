cb80766b9f2c36565873dc6287732a48590dc53c
Canaga81 <Canagamemmedov@gmail.com>
2024-01-31 11:43:39 +0400

    Unity Component Style Finish

--- src/pages/Unity/Unity.jsx
+++ src/pages/Unity/Unity.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import './Unity.css'
+import React from "react";
+import "./Unity.css";
+import unityCardsData from "../../components/UnityCardsData";
 
 const Unity = () => {
   return (
-    <div>Unity</div>
-  )
-}
+    <div className="unity">
+      <div className="unity_container">
+        <div className="unity_top_container">
+          <h2 className="unity-h2">Xidmət Etdiyimiz Sənayelər</h2>
+        </div>
+        <div className="unity_bottom_container">
+          {unityCardsData.map((item, idx) => {
+            return (
+              <div className="unity_card" key={idx}>
+                <div className="unity_icon_bg">
+                  <div className="unity_icon-a">{item.icon}</div>
+                </div>
 
-export default Unity
+                <div className="unity_text_cont">
+                  <h4 className="unity_title-h4">{item.title}</h4>
+                  <p className="unity_desc-p">{item.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Unity;
