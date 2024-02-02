dc73a1b361a602fcb516f936512d6c9255dd9735
Canaga81 <Canagamemmedov@gmail.com>
2024-02-02 10:55:34 +0400

    Question & OurServices Components Change Style

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './Questions.css';
+import SectionHead from '../SectionHead/SectionHead';
+import { FaQuestion } from "react-icons/fa";
+import faqs from '../QuestionsData';
+import Question from '../Question/Question';
 
 const Questions = () => {
     
   return (
-    <div>Questions</div>
+    <div>
+      <section className="faqs">
+      <div className="container faqs__container">
+        <SectionHead icon={<FaQuestion/>} title='FAQs'/>
+        <div className="faqs__wrapper">
+          {
+            faqs.map(({id,question,answer})=>{
+              return <Question key={id} question={question} answer={answer}/>
+            })
+          }
+        </div>
+      </div>
+    </section>
+    </div>
+
   )
   
 }
 
-export default Questions
+export default Questions;
